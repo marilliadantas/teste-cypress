29c7a6e4f4d25cb87dcd6653cb87ee5cfa27df32
Marillia Dantas <marillia.dantas@hotmail.com>
2023-11-30 15:29:25 -0300

    config importantes

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    // viewportWidth: 1920,
+    // viewportHeight: 1080,
+    // video: false,
+    // screenshotOnRunFailure: false,
+    // chromeWebSecurity: false,
+    // baseUrl: "https://walkdog.vercel.app/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
